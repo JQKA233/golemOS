1ec8426de381e5e4e256b9e634b42bb2f93f2d00
JQKA233 <852576001@qq.com>
2024-04-28 20:41:40 +0800

    first run

---
+++ Main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout<<"first run"<<std::endl;
+    return 0;
+}
---
+++ build/CMakeFiles/GOLEM.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GOLEM.dir/Main.cpp.o"
+  "GOLEM"
+  "GOLEM.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GOLEM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
